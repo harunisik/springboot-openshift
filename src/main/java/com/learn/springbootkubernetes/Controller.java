7fcb6da077445cff1caee4e56797c4fecbb0b6e2
Harun Isik <harun1711@gmail.com>
2021-03-01 11:07:21 +0000

    mehtod log is added.

--- src/main/java/com/learn/springbootkubernetes/Controller.java
+++ src/main/java/com/learn/springbootkubernetes/Controller.java
@@ -1,5 +1,6 @@
 package com.learn.springbootkubernetes;
 
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    private static final Logger logger = Logger.getLogger(Controller.class.getName());
+
     @Value("${name}")
     private String name;
 
     @RequestMapping("/")
     public String index() {
+        logger.info("index method method has been called !");
         return "Hello " + name + ", Spring-Boot app successfully deployed and running on Minishift";
     }
 }
