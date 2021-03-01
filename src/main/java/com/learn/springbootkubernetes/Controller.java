40780fa14f1b1c9b52f3ae9cceb93389dccd0aec
Harun Isik <harun1711@gmail.com>
2021-03-01 10:40:19 +0000

    name prop is added.

--- src/main/java/com/learn/springbootkubernetes/Controller.java
+++ src/main/java/com/learn/springbootkubernetes/Controller.java
@@ -1,12 +1,17 @@
 package com.learn.springbootkubernetes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @Value("${name}")
+    private String name;
+
     @RequestMapping("/")
     public String index() {
-        return "Hello Spring-Boot app successfully deployed and running on Minishift";
+        return "Hello " + name + ", Spring-Boot app successfully deployed and running on Minishift";
     }
 }
