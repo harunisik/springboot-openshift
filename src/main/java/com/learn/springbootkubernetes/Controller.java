0ce5ef9bfe2a0ca6df15e3f3ac48045362439816
Harun Isik <harun1711@gmail.com>
2021-03-02 11:51:24 +0000

    DB conn example added

--- src/main/java/com/learn/springbootkubernetes/Controller.java
+++ src/main/java/com/learn/springbootkubernetes/Controller.java
@@ -1,6 +1,12 @@
 package com.learn.springbootkubernetes;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +19,32 @@ public class Controller {
     @Value("${name}")
     private String name;
 
+
     @RequestMapping("/")
     public String index() {
         logger.info("index method method has been called !");
-        return "Hello " + name + ", Spring-Boot app successfully deployed and running on Minishift";
+        List<String> tableNames = new ArrayList<>();
+        try {
+            tableNames = showTables();
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+        return "Hello " + name + ", Spring-Boot app successfully deployed....."
+            + "Sys tables: " + tableNames;
+    }
+
+    @Autowired
+    protected DataSource dataSource;
+
+    public List<String> showTables() throws Exception {
+        DatabaseMetaData metaData = dataSource.getConnection().getMetaData();
+        ResultSet tables = metaData.getTables(null, null, null, new String[]{"TABLE"});
+        List<String> tableNames = new ArrayList<>();
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            tableNames.add(tableName);
+        }
+        logger.info("Table List: " + tableNames.toString());
+        return tableNames;
     }
 }
